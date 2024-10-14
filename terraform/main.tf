fe67aad6ab75eff5d9d1de6f75571fc3ad2a462b
vidhya-1160074 <Vidhya.Mohan@lincolnuni.ac.nz>
2024-10-14 21:17:38 +1300

    fixing aws issues Updated the terraform and task defention

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,56 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+# Create IAM Role for ECS Task Execution
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "spiceCraftEcsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = {
+    Name = "ecs_task_execution_role"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# Create IAM Role for ECS Task
+resource "aws_iam_role" "ecs_task_role" {
+  name = "spiceCraftEcsTaskRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = {
+    Name = "ecs_task_role"
+  }
+}
+
 # Create ECS Task Definition for Fargate with EFS Mount
 resource "aws_ecs_task_definition" "spicecraft_task" {
   family                   = "spicecraft-task"
